119cb9ec96dbbaf3a9b0ea3a3ab58ec4d778e251
Anton Mokin <ay.mokin@sollers-auto.com>
2021-11-28 22:53:51 +0700

    add some variables for detecting https scheme (ingress kubernetes)

--- app/www/index.php
+++ app/www/index.php
@@ -8,8 +8,11 @@ require_once(__DIR__ . '/../config.php');
 if ((isset($_SESSION['oauth_gitlab']) and $_SESSION['oauth_gitlab'] ) or defined("PASSWORD")){
     echo file_get_contents('index.html');
 }else{
-    $link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ?
-                "https" : "http") . "://" . $_SERVER['HTTP_HOST'] . 
+    $link = (   (  (isset($_SERVER['HTTPS'])         && $_SERVER['HTTPS'] === 'on' ) 
+                || (isset($_SERVER['HTTP_X_SCHEME']) && $_SERVER['HTTP_X_SCHEME'] == 'https' )
+                || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' )
+                )
+                ? "https" : "http") . "://" . $_SERVER['HTTP_HOST'] . 
                 $_SERVER['REQUEST_URI'];
   
     header('Location: auth/gitlab.php?'.http_build_query(["return"=>$link]));
